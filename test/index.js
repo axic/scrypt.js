55293da64dfc31fdd0661c3fc8ea8abdb1bee4f3
Alex Beregszaszi <alex@rtfs.hu>
2018-12-18 12:49:59 +0000

    Add test for auto-detected case

--- test/index.js
+++ test/index.js
@@ -1,6 +1,14 @@
 const tape = require('tape')
+const scrypt = require('../index')
 const scryptJS = require('../js')
 const scryptNode = require('../node')
+
+tape('Auto-detected', function (t) {
+  t.test('basic', function (st) {
+    st.deepEqual(scrypt(Buffer.from('key'), Buffer.from('salt'), 1024, 256, 4, 16), Buffer.from('fcc68e0894929cb761fadd8990e0946b', 'hex'))
+    st.end()
+  })
+})
 
 tape('Javascript', function (t) {
   t.test('basic', function (st) {
