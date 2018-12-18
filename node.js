4573776ea60c70faf1f466635c75e937a297c20c
Alex Beregszaszi <alex@rtfs.hu>
2018-12-18 12:33:53 +0000

    Use standard for lint

--- node.js
+++ node.js
@@ -1,6 +1,6 @@
 var scrypt = require('scrypt')
 
-function hash(key, salt, n, r, p, dklen, progressCb) {
+function hash (key, salt, n, r, p, dklen, progressCb) {
   return scrypt.hashSync(key, { N: n, r: r, p: p }, dklen, salt)
 }
 
